80c9d4dc7a301fccdafebae50b2740f6d196972f
Surekha Rane <surekha@ajency.in>
2015-03-16 12:06:07 +0530

    user post likes

--- wp-content/themes/short-film/classes/class.video.php
+++ wp-content/themes/short-film/classes/class.video.php
@@ -33,7 +33,7 @@ class Video
 			if($name == " ")
 				$name = get_user_details($post->post_author)->data->display_name;
 
-			$post_user_like = (!get_user_details($post->post_author)) ? "" :get_user_details($post->post_author)->user_like_count;
+			$post_user_like = (!get_user_details($post->post_author)) ? "" :get_user_details($post->post_author)->data->user_like_count;
 			$post_thumbnail_id = get_post_thumbnail_id($post->ID); 
 			$image_details = wp_get_attachment_image_src( $post_thumbnail_id, 'medium');
 			$image = is_array( $image_details ) && count( $image_details ) > 1 ? $image_details[ 0 ] : get_template_directory_uri() .
