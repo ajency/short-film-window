fe11a2445983f8ecfe1e519a394179dcf2961fc3
Surekha Rane <surekha@ajency.in>
2015-03-16 10:46:03 +0530

    blank firstname and lastname

--- wp-content/themes/short-film/classes/class.video.php
+++ wp-content/themes/short-film/classes/class.video.php
@@ -28,10 +28,10 @@ class Video
 			$name = (!get_user_details($post->post_author)) ? "" :
 						 get_user_meta(get_user_details($post->post_author)->ID,'first_name' , true).' '.
 						 get_user_meta(get_user_details($post->post_author)->ID,'last_name' , true);
-
-			if($name == "")
-				$name = get_user_details($post->post_author)->display_name;
 			
+			if($name == "")
+				$name = get_user_details($post->post_author)->data->display_name;
+
 			$post_user_like = (!get_user_details($post->post_author)) ? "" :get_user_details($post->post_author)->user_like_count;
 			$post_thumbnail_id = get_post_thumbnail_id($post->ID); 
 			$image_details = wp_get_attachment_image_src( $post_thumbnail_id, 'medium');
