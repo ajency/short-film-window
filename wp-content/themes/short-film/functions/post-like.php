a071c355f4dd40fc6dd0ea515f05540c05df7056
Jeromie Fernandes <jeromie@ajency.in>
2015-03-16 13:45:07 +0530

    footer links alignment #57

--- wp-content/themes/short-film/functions/post-like.php
+++ wp-content/themes/short-film/functions/post-like.php
@@ -187,13 +187,13 @@ function getPostLikeLink( $post_id ) {
 	if ( AlreadyLiked( $post_id ) ) {
 		$class = esc_attr( ' liked' );
 		$title = esc_attr( 'Unlike' );
-		$heart = '<i id="icon-like" class="icon-like"></i>';
+		$heart = '<i id="icon-like" class="fa fa-thumbs-up"></i>';
 	} else {
 		$class = esc_attr( '' );
 		$title = esc_attr( 'Like' );
-		$heart = '<i id="icon-unlike" class="icon-unlike"></i>';
+		$heart = '<i id="icon-unlike" class="fa fa-thumbs-up"></i>';
 	}
-	$output = '<a href="#" class="jm-post-like'.$class.'" data-post_id="'.$post_id.'" title="'.$title.'">'.$heart.'&nbsp;'.$count.'</a>';
+	$output = '<a href="#" class="'.$class.'" data-post_id="'.$post_id.'" title="'.$title.'">'.$heart.'&nbsp;'.$count.'</a>';
 	return $output;
 }
 
