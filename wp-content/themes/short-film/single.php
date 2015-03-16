7020472505bff66a605a3a0a4cb61c92ec446511
Surekha Rane <surekha@ajency.in>
2015-03-16 13:45:42 +0530

    link for in focus film

--- wp-content/themes/short-film/single.php
+++ wp-content/themes/short-film/single.php
@@ -235,6 +235,8 @@ window.onload = function() {
     	console.log(response);
 
     	html = '<div class="row">'
+    	+'<a class="content-bottom" target="_blank" href="'+SITEURL+'/'+value.slug+'">'
+	         
 	        +'<div class="col-md-6">'
 	            +'<div class="pull-left">'
 	                +'<h4>IN FOCUS: <small><em>'+response.title+'</em></small></h4>'
@@ -265,6 +267,7 @@ window.onload = function() {
 	            +'</div>'
 	           +' <div class="clearfix"></div>'
 	        +'</div>'
+	        +'</a>'
 	    +'</div>';
 
 	    jQuery('.infocus').html(html);
