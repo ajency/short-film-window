6f1fbee2efbef84a4b2bf4c6568c5b3993447f3a
shortfilmwindow <shortfilmwindow@maat.dreamhost.com>
2015-04-28 05:59:48 -0700

    removed formidable

--- wp-content/plugins/formidable/pro/classes/views/xml/items_xml.php
+++ wp-content/plugins/formidable/pro/classes/views/xml/items_xml.php
@@ -1,19 +1,18 @@
-<?php 
+<?php
 
-if ( !$item_ids )
+if ( ! $item_ids ) {
     return;
+}
 $item_form_id = 0;
 
 // fetch 20 posts at a time rather than loading the entire table into memory
 while ( $next_set = array_splice( $item_ids, 0, 20 ) ) {
-$where = 'WHERE id IN (' . join( ',', $next_set ) . ')';
-$entries = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}frm_items $where" );
-unset($where);
+$entries = FrmDb::get_results( 'frm_items', array( 'or' => 1, 'id' => $next_set, 'parent_item_id' => $next_set ) );
 
 // Begin Loop
 foreach ( $entries as $entry ) {
-    if($item_form_id != $entry->form_id){
-        $fields = $frm_field->getAll(array('fi.form_id' => $entry->form_id), 'field_order');
+	if ( $item_form_id != $entry->form_id ) {
+		$fields = FrmField::get_all_for_form( $entry->form_id );
         $item_form_id = $entry->form_id;
     }
 ?>
@@ -31,18 +30,20 @@ foreach ( $entries as $entry ) {
 		<user_id><?php echo FrmXMLHelper::cdata(FrmProFieldsHelper::get_display_name($entry->user_id, 'user_login')); ?></user_id>
 		<updated_by><?php echo FrmXMLHelper::cdata(FrmProFieldsHelper::get_display_name($entry->updated_by, 'user_login')); ?></updated_by>
         <parent_item_id><?php echo $entry->parent_item_id ?></parent_item_id>
-        
+
 <?php
-		$metas = $wpdb->get_results( $wpdb->prepare("SELECT meta_value, field_id FROM {$wpdb->prefix}frm_item_metas WHERE item_id=%d", $entry->id ));
+        $metas = FrmDb::get_results( $wpdb->prefix .'frm_item_metas', array( 'item_id' => $entry->id), 'meta_value, field_id' );
+
 		foreach ( $metas as $meta ){ ?>
 		<item_meta>
 		    <field_id><?php echo $meta->field_id ?></field_id>
 		    <meta_value><?php
-		        if(isset($fields[$meta->field_id]))
-		            $meta->meta_value = FrmProFieldsHelper::get_export_val($meta->meta_value, $fields[$meta->field_id]);
-		        
+		        if ( isset( $fields[ $meta->field_id ] ) ) {
+		            $meta->meta_value = FrmProFieldsHelper::get_export_val( $meta->meta_value, $fields[ $meta->field_id ] );
+                }
+
 		        echo FrmXMLHelper::cdata($meta->meta_value);
-		        
+
 		        unset($meta);
 		    ?></meta_value>
 		</item_meta>
@@ -50,18 +51,18 @@ foreach ( $entries as $entry ) {
 	</item>
 <?php
     unset($metas);
-    
-    if(!empty($entry->post_id)){
+
+    if ( ! empty( $entry->post_id ) ) {
         $old_ids = $item_ids;
         $item_ids = array($entry->post_id);
-        include(dirname(__FILE__) .'/views_xml.php');
+        include(FrmAppHelper::plugin_path() .'/classes/views/xml/posts_xml.php');
         $item_ids = $old_ids;
     }
-    
+
     unset($entry);
 }
 }
 
-if(isset($fields))
+if ( isset($fields) ) {
     unset($fields);
-?>
+}
