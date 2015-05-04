bf30081aa0b60cf147a69bf8818807bab376efd3
nivedita <nivedita@ajency.in>
2015-05-04 19:11:24 +0530

    author template changes

--- wp-content/themes/short-film/author.php
+++ wp-content/themes/short-film/author.php
@@ -4,21 +4,36 @@
 			
 				<div id="main" class="col-sm-8 clearfix" role="main">
 				
-					<div class="page-header"><h1 class="archive_title h2">
-						<span><?php _e("Posts By:", "wpbootstrap"); ?></span> 
-						<?php 
-							// If google profile field is filled out on author profile, link the author's page to their google+ profile page
-							$curauth = (get_query_var('author_name')) ? get_user_by('slug', get_query_var('author_name')) : get_userdata(get_query_var('author'));
-							$google_profile = get_the_author_meta( 'google_profile', $curauth->ID );
-							if ( $google_profile ) {
-								echo '<a href="' . esc_url( $google_profile ) . '" rel="me">' . $curauth->display_name . '</a>'; 
-						?>
-						<?php 
-							} else {
-								echo get_the_author_meta('display_name', $curauth->ID);
-							}
-						?>
-					</h1></div>
+					<div class="page-header">
+						<h1 class="archive_title h2">
+							
+							<!-- <span><?php _e("Posts By:", "wpbootstrap"); ?></span> -->
+							
+							<?php 
+								// If google profile field is filled out on author profile, link the author's page to their google+ profile page
+								$curauth = (get_query_var('author_name')) ? get_user_by('slug', get_query_var('author_name')) : get_userdata(get_query_var('author'));
+								
+								// echo "hii";
+								// print_r($curauth);
+								 // print_r($curauth->ID);
+								  // print_r($curauth->display_name);
+								
+								$google_profile = get_the_author_meta( 'google_profile', $curauth->ID );
+								
+								if ( $google_profile ) 
+								{
+									echo '<a href="' . esc_url( $google_profile ) . '" rel="me">' . $curauth->display_name . '</a>'; 
+							?>
+							<?php 
+								} 
+								else
+								{
+									$author_display_name = get_the_author_meta('display_name', $curauth->ID);
+									echo $author_display_name;  // <-
+								}
+							?>
+						</h1>
+					</div>
 					
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
@@ -79,7 +94,7 @@
 			
 				</div> <!-- end #main -->
     
-				<?php get_sidebar(); // sidebar 1 ?>
+				<?php// get_sidebar(); // sidebar 1 ?>
     
 			</div> <!-- end #content -->
 
