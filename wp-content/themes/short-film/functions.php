bb4b57dcd21fc90ed44f85eb54b39808bd5b5171
nivedita <nivedita@ajency.in>
2015-05-04 12:02:16 +0530

    like button on single article page( added & included post-like.js)

--- wp-content/themes/short-film/functions.php
+++ wp-content/themes/short-film/functions.php
@@ -647,7 +647,7 @@ function add_custom_scripts() {
     wp_register_script( 'custom_js', get_template_directory_uri() . '/assets/js/custom.js', '', false, true );
     wp_enqueue_script( 'custom_js' );
 
-    wp_register_script( 'like_js', get_template_directory_uri(). '/assets/js/post-like.min.js');
+    wp_register_script( 'like_js', get_template_directory_uri(). '/assets/js/post-like.js');
     wp_enqueue_script( 'like_js' );
 
      wp_register_script( 'scroll_js', get_template_directory_uri(). '/assets/js/jquery.infinitescroll.min.js');
