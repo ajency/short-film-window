944464ebc25bd1fb06cf6a833ed9955e797f9cb7
namrata salaskar <namrata@ajency.in>
2016-01-12 15:15:14 +0530

    dynamic height .

--- www/app/MyWatchlist/MyWatchList.js
+++ www/app/MyWatchlist/MyWatchList.js
@@ -1,13 +1,20 @@
 angular.module('SFWApp.watchlist', []).controller('watchlistCtrl', [
-  '$scope', 'Storage', function($scope, Storage) {
+  '$scope', 'Storage', 'DetailsAPI', 'App', '$window', function($scope, Storage, DetailsAPI, App, $window) {
     $scope.watchlistDetails = [];
     $scope.display = 'loader';
-    return $scope.init = function() {
+    $scope.init = function() {
       console.log("watch list ");
       return Storage.watchlistDetails('get').then(function(value) {
+        var device_height, device_width;
         console.log(value);
         $scope.watchlistDetails = value;
         if ($scope.watchlistDetails.length > 0) {
+          device_width = $window.innerWidth;
+          device_height = $window.innerHeight;
+          console.log(device_width);
+          console.log(device_height);
+          $scope.used_height = 43 + 72;
+          $scope.hgt = device_height - $scope.used_height;
           $scope.display = 'result';
           return $scope.$apply();
         } else {
@@ -15,6 +22,13 @@ angular.module('SFWApp.watchlist', []).controller('watchlistCtrl', [
           return $scope.$apply();
         }
       });
+    };
+    return $scope.singleplay = function(videoid) {
+      console.log(videoid);
+      DetailsAPI.videoId = videoid;
+      console.log(DetailsAPI.videoId);
+      console.log("enterd single play .");
+      return App.navigate('init');
     };
   }
 ]);
