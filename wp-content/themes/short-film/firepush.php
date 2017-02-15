20f7b782ded805b1bb00066ec6d4995123d54168
Antonio Furtado <antonio@ajency.in>
2017-02-15 14:43:06 +0530

    Added test settings

--- wp-content/themes/short-film/firepush.php
+++ wp-content/themes/short-film/firepush.php
@@ -1,7 +1,17 @@
 <?php
 
-define( 'API_ACCESS_KEY', 'AAAAdFkKiRM:APA91bHakngVy9EGqjjyCHXI3m_oDPeTU2RJaFHvfTmKUsQjxGoxu71xxwCy1qppNlZ0fRzei8u22UF0sovmv0OC_gw3jE7MvjaFscSypg1wwuakSNLE8MRpMf7eFjeQNNWD8e5QpU62' );
-define( 'NOTIFICATIONS_STORE_DB', "https://sfwindow-b3160.firebaseio.com/notifications.json");
+$test = true;
+if($test){
+    $api_access_key = 'AIzaSyCVSA3tkbBp7Sk_4HB3GYnfFL0u3XUbJfk';
+    $notification_storage_db = 'https://shortfilmwindow-e5571.firebaseio.com/notifications.json';
+} else {
+    $api_access_key = 'AIzaSyCIzwFzGdQUCc_CXpo7WfW8rg_5kHyQjfU';
+    $notification_storage_db = 'https://sfwindow-b3160.firebaseio.com/notifications.json';
+}
+
+//define( 'API_ACCESS_KEY', 'AAAAdFkKiRM:APA91bHakngVy9EGqjjyCHXI3m_oDPeTU2RJaFHvfTmKUsQjxGoxu71xxwCy1qppNlZ0fRzei8u22UF0sovmv0OC_gw3jE7MvjaFscSypg1wwuakSNLE8MRpMf7eFjeQNNWD8e5QpU62' );
+define( 'API_ACCESS_KEY', $api_access_key);
+define( 'NOTIFICATIONS_STORE_DB', $notification_storage_db);
 define( 'FCM_SEND_URL', "https://fcm.googleapis.com/fcm/send");
 define( 'TOPIC_ANDROID', "android");
 define( 'TOPIC_ANDROID_KEY', "data");
@@ -14,7 +24,6 @@ class firePush
     {
         if($topic)
         {
-
             $ch = curl_init(FCM_SEND_URL);
 
             $token = '/topics/'.$topic;
